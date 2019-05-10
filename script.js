808ccb6090fcc86118e25ce0c0a129bb55cf72c7
Alejandro Martinez <j.alejandro.mrtnz@gmail.com>
2019-05-10 14:49:02 -0700

    finish app functionality

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var gridContainer = document.querySelector('.grid-container');
+    var colors = document.querySelector('.colors');
+    var color = "white";
+    var colorIndicator = document.querySelector("#color-indicator");
+
+    colors.addEventListener('click', function (event) {
+        const paletteColor = getComputedStyle(event.target).backgroundColor;
+        color = paletteColor;
+        colorIndicator.style.backgroundColor = color;
+
+    })
+
+    gridContainer.addEventListener('click', function (event) {
+        event.target.style.backgroundColor = color;
+        
+    });
+
+})
+
